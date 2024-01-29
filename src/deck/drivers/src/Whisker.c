42b1e01769768716468c9ff00ebcc910f5ac8cb4
TacYee <450356982@qq.com>
2024-01-29 17:55:37 +0100

    update 2whisker data reading

--- src/deck/drivers/src/Whisker.c
+++ src/deck/drivers/src/Whisker.c
@@ -8,6 +8,7 @@
 #include "log.h"
 #include "deck.h"
 #include "uart1.h"
+#include "uart2.h"
 #include "FreeRTOS.h"
 #include "task.h"
 #include "system.h"
@@ -16,9 +17,9 @@
 
 static uint8_t isInit = 0;
 // uint32_t loggedTimestamp = 0;
-float barometer1, barometer2, barometer3;
+float barometer1_1, barometer1_2, barometer1_3, barometer2_1, barometer2_2, barometer2_3; 
 
-void readSerial() {
+void readSerial1() {
     char buf[MAX_MESSAGE_SIZE];
     char c = '0';
     char* token = NULL;
@@ -36,13 +37,13 @@ void readSerial() {
             while (token != NULL && tokenCount < 3) {
                 switch (tokenCount) {
                     case 0:
-                        barometer1 = atof(token);
+                        barometer1_1 = atof(token);
                         break;
                     case 1:
-                        barometer2 = atof(token);
+                        barometer1_2 = atof(token);
                         break;
                     case 2:
-                        barometer3 = atof(token);
+                        barometer1_3 = atof(token);
                         break;
                 }
 
@@ -51,7 +52,56 @@ void readSerial() {
             }
 
             if (tokenCount < 3) {
-                DEBUG_PRINT("Error parsing sensor data\n");
+                DEBUG_PRINT("Error parsing sensor1 data\n");
+            }
+
+            break;
+        }
+
+        buf[i] = c;
+
+        if (c == ',') {
+            // 重置 token 和 tokenCount
+            token = NULL;
+            tokenCount = 0;
+        }
+    }
+}
+
+void readSerial2() {
+    char buf[MAX_MESSAGE_SIZE];
+    char c = '0';
+    char* token = NULL;
+    int tokenCount = 0;
+
+    for (int i = 0; i < MAX_MESSAGE_SIZE - 1; i++) {
+        if (!uart2GetCharWithDefaultTimeout(&c)) break;
+
+        if (c == '\n') {
+            buf[i] = '\0';
+            DEBUG_PRINT("%s\n", buf);
+
+            // 使用strtok分割字符串
+            token = strtok(buf, ",");
+            while (token != NULL && tokenCount < 3) {
+                switch (tokenCount) {
+                    case 0:
+                        barometer2_1 = atof(token);
+                        break;
+                    case 1:
+                        barometer2_2 = atof(token);
+                        break;
+                    case 2:
+                        barometer2_3 = atof(token);
+                        break;
+                }
+
+                token = strtok(NULL, ",");
+                tokenCount++;
+            }
+
+            if (tokenCount < 3) {
+                DEBUG_PRINT("Error parsing sensor2 data\n");
             }
 
             break;
@@ -71,13 +121,17 @@ void readSerial() {
 void WhiskerTask(void *param) {
     systemWaitStart();
 
-    while (1) readSerial();
+    while (1) {
+        readSerial1();
+        readSerial2();
+    }
 }
 
 static void WhiskerInit() {
     DEBUG_PRINT("Initialize driver\n");
 
     uart1Init(115200);
+    uart2Init(115200);
 
     xTaskCreate(WhiskerTask, WHISKER_TASK_NAME, WHISKER_TASK_STACKSIZE, NULL,
                 WHISKER_TASK_PRI, NULL);
@@ -93,7 +147,7 @@ static const DeckDriver WhiskerDriver = {
         .name = "Whisker",
         .init = WhiskerInit,
         .test = WhiskerTest,
-        .usedPeriph = DECK_USING_UART1,
+        .usedPeriph = DECK_USING_UART1 | DECK_USING_UART2,
 };
 
 DECK_DRIVER(WhiskerDriver);
@@ -103,7 +157,10 @@ DECK_DRIVER(WhiskerDriver);
  * Logging variables for the Whisker
  */
 LOG_GROUP_START(Whisker)
-LOG_ADD(LOG_FLOAT, Barometer1, &barometer1)
-LOG_ADD(LOG_FLOAT, Barometer2, &barometer2)
-LOG_ADD(LOG_FLOAT, Barometer3, &barometer3)
+LOG_ADD(LOG_FLOAT, Barometer1_1, &barometer1_1)
+LOG_ADD(LOG_FLOAT, Barometer1_2, &barometer1_2)
+LOG_ADD(LOG_FLOAT, Barometer1_3, &barometer1_3)
+LOG_ADD(LOG_FLOAT, Barometer2_1, &barometer2_1)
+LOG_ADD(LOG_FLOAT, Barometer2_2, &barometer2_2)
+LOG_ADD(LOG_FLOAT, Barometer2_3, &barometer2_3)
 LOG_GROUP_STOP(Whisker) 
